bf6de3a78227e81500adfc9ac8ba2ad0985a0be0
Faithful Samuel <faithfulsam350@gmail.com>
2025-09-03 15:24:59 +0000

    test

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,3 +1,4 @@
 // This file is deprecated. Use the new data structure from /src/data/
 // Re-export for backward compatibility
 export { projects, blogPosts, technicalSkills, softSkills, tools, experiences } from '@/data';
+// fhf
