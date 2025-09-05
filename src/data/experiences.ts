10157f7eadf1c761ebcc5551f3d355adfc56a00f
Faithful Samuel <faithfulsam350@gmail.com>
2025-09-05 15:41:43 +0000

    remove this section (_for element div_)

--- src/data/experiences.ts
+++ src/data/experiences.ts
@@ -7,10 +7,4 @@ export const experiences: Experience[] = [
     period: '2025',
     description: 'Designed and developed website and dashboard experiences, including information architecture, wireframes, and high-fidelity prototypes.|Contributed to improving usability and creating a consistent design system across projects.'
   },
-  {
-    role: 'Product Designer',
-    company: 'Yapish - Personal Project',
-    period: '2024 - Present',
-    description: 'Developing a podcast platform concept with a focus on simplified UX, branding, and a clean interface to address the cluttered experience of existing apps.'
-  },
 ];
