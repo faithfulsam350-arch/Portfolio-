13e2cc809a29b9af5125418abf25704a39ba3eb6
Faithful Samuel <faithfulsam350@gmail.com>
2025-09-05 16:11:04 +0000

    remove develped (_for element li_)

--- src/data/experiences.ts
+++ src/data/experiences.ts
@@ -5,6 +5,6 @@ export const experiences: Experience[] = [
     role: 'Product Designer Intern',
     company: 'Omnicom Solutions',
     period: '2025',
-    description: 'Designed and developed website and dashboard experiences, including information architecture, wireframes, and high-fidelity prototypes.|Contributed to improving usability and creating a consistent design system across projects.'
+    description: 'Designed website and dashboard experiences, including information architecture, wireframes, and high-fidelity prototypes.|Contributed to improving usability and creating a consistent design system across projects.'
   },
 ];
