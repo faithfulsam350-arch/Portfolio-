5d78b5c3bb69449a2c6fa8c103d396a2e62499b2
Faithful Samuel <faithfulsam350@gmail.com>
2025-09-05 15:39:14 +0000

    remove this section (_for element div_)

--- src/data/experiences.ts
+++ src/data/experiences.ts
@@ -8,12 +8,6 @@ export const experiences: Experience[] = [
     description: 'Designed and developed website and dashboard experiences, including information architecture, wireframes, and high-fidelity prototypes.|Contributed to improving usability and creating a consistent design system across projects.'
   },
   {
-    role: 'Product Designer',
-    company: 'Muslim Finance App',
-    period: '2023 - Present',
-    description: 'Led the end-to-end design of an MVP for a fintech app, including user research, wireframing, UI design, and prototyping to create a user-friendly financial management tool.'
-  },
-  {
     role: 'UI/UX Designer',
     company: 'Yapish - Personal Project',
     period: '2024 - Present',
