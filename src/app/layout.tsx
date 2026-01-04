d1a740072b8b7c6d460cb368598b74a09ba14310
Faithful Samuel <faithfulsam350@gmail.com>
2026-01-04 09:09:43 +0000

    In the contact page remove all the footer I don't want anything there

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { cn } from '@/lib/utils';
 import { Header } from '@/components/layout/header';
-import { Footer } from '@/components/layout/footer';
 import { Toaster } from "@/components/ui/toaster"
 
 
@@ -31,7 +30,6 @@ export default function RootLayout({
         <div className="relative flex min-h-dvh flex-col bg-background">
           <Header />
           <main className="flex-1">{children}</main>
-          <Footer />
         </div>
         <Toaster />
       </body>
